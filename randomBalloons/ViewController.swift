2787d162cce4d018f63a171833b1793a4d4aacfd
ankurchdry <er.ankur.choudhary@gmail.com>
2014-10-05 23:07:18 -0400

    very unstructured implementation

--- randomBalloons/ViewController.swift
+++ randomBalloons/ViewController.swift
@@ -10,14 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfBalloons: UILabel!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    var myBalloonsImage:[balloons] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+     var myBalloons = balloons()
+         myBalloons.numberOfBalloons = 1
+         myBalloons.image = UIImage (named: "RedBalloon1.jpg")
+        
+        numberOfBalloons.text = "\(myBalloons.numberOfBalloons)"
+        myImageView.image = myBalloons.image
+        
+        var secondBalloons = balloons()
+        secondBalloons.numberOfBalloons = 2
+        secondBalloons.image = UIImage (named: "RedBalloon2.jpg")
+        
+        var thirdBalloons = balloons()
+        thirdBalloons.numberOfBalloons = 3
+        thirdBalloons.image = UIImage (named: "RedBalloon3.jpg")
+        
+        var fourthBalloons = balloons()
+        fourthBalloons.numberOfBalloons = 4
+        fourthBalloons.image = UIImage (named: "RedBalloon4.jpg")
+        
+        myBalloonsImage += [myBalloons,secondBalloons,thirdBalloons,fourthBalloons]
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func whenNextButtonPressed(sender: UIBarButtonItem) {
+        
+        var randomindex = Int(arc4random_uniform(UInt32(myBalloonsImage.count)))
+        let nextBalloonPic = myBalloonsImage[randomindex]
+        
+        numberOfBalloons.text = "\(nextBalloonPic.numberOfBalloons)"
+        myImageView.image = nextBalloonPic.image
+        
     }
 
 
---
+++ randomBalloons/balloons.swift
@@ -0,0 +1,20 @@
+//
+//  balloons.swift
+//  randomBalloons
+//
+//  Created by ankurchdry on 10/5/14.
+//  Copyright (c) 2014 ankurchdry. All rights reserved.
+//
+
+import Foundation
+import UIkit
+
+
+struct balloons {
+    
+   var numberOfBalloons = 0
+    
+   var image = UIImage (named: "")
+    
+
+}
